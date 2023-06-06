76342e22fe76c04cb0ca746302097c899246f8f8
phinsso <naso01247@gmail.com>
2023-06-06 09:16:07 +0900

    feat: 프로젝트 기본 설정

---
+++ src/main/java/org/fire/YourtoodayApplication.java
@@ -0,0 +1,11 @@
+package org.fire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourtoodayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YourtoodayApplication.class, args);
+    }
+}
