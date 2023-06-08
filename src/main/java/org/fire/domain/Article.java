4abd814fdd838300c8be5ee483476eda15a37fed
phinsso <naso01247@gmail.com>
2023-06-09 01:12:28 +0900

    feat: 일기 엔티티 구성 / 리포지토리 생성

---
+++ src/main/java/org/fire/domain/Article.java
@@ -0,0 +1,38 @@
+package org.fire.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "article_no", updatable = false)
+    private Long articleNo; // 기본키
+
+    @Column(name = "title", nullable = false)
+    private String title; // 일기 제목
+
+    @Column(name = "content", nullable = false)
+    private String content; // 일기 내용
+
+    @Column(name = "author", nullable = false)
+    private String author; // 일기 작성자
+
+    @Column(name = "date", nullable = false)
+    private String date; // 일기 작성 날짜
+
+    @Builder
+    public Article(String title, String content, String author, String date) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+        this.date = date;
+    }
+}
