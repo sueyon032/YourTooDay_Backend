70614a252c4055d6723e97682facfd3fe97d6e19
phinsso <naso01247@gmail.com>
2023-10-14 16:05:10 +0900

    feat: cors 설정 추가

---
+++ src/main/java/org/fire/configuration/CorsConfiguration.java
@@ -0,0 +1,20 @@
+package org.fire.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Authorization", "Content-Type")
+                .exposedHeaders("Custom-Header")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
